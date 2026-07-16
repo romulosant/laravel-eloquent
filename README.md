2057a9605eb757f2511e9672b5f90963de195cdb
Romulo <romulosantos.62014@gmail.com>
2026-07-15 23:17:32 -0300

    iniciando

---
+++ README.md
@@ -0,0 +1,40 @@
+## Projeto desenvolvido no curso de Laravel Fundamentos
+
+Desenvolvido no curso de Laravel Fundamentos
+
+### Instalando o projeto
+
+#### Clonar o repositório
+
+```
+git clone https://github.com/treinaweb/treinaweb-laravel-fundamentos-v2.git
+```
+
+#### Instalar as depencências
+
+```
+composer install
+```
+
+Ou em ambiente de desenvolvimento:
+
+```
+composer update
+```
+
+#### Criar arquivo de configurações de ambiente
+
+Copiar o arquivo de exemplo `.env.example` para `.env` na raiz do projeto
+configurar os detalhes da aplicação e conexão com o banco de dados.
+
+#### Criar a estrutura no banco de dados
+
+```
+php artisan migrate
+```
+
+#### Iniciar o servidor de desenvolvimento
+
+```
+php artisan serve
+```
---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Lista os clientes do banco de dados
+     * 
+     * @return View|Factory 
+     */
+    public function index()
+    {
+        $clientes = Client::get();
+
+        return view('clientes.index', [
+            'clientes' => $clientes
+        ]);
+    }
+
+    /**
+     * Mostra o formulário de cadastro de clientes
+     * 
+     * @return View|Factory 
+     */
+    public function create()
+    {
+        return view('clientes.create');
+    }
+
+    /**
+     * Grava o cliente no banco de dados
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => ['required', 'min:3', 'max:100'],
+            'endereco' => ['required', 'max:200'],
+            'descricao' => ['required']
+        ]);
+
+        $novoCliente = new Client;
+        $novoCliente->nome = $request->input('nome');
+        $novoCliente->endereco = $request->input('endereco');
+        $novoCliente->descricao = $request->input('descricao');
+
+        $novoCliente->save();
+
+        return redirect('/clientes');
+    }
+}
